ae8277189ea5d283ac87513d41bc84ffee8317af
Mee321 <michao321@163.com>
2019-04-21 19:23:47 +0800

    DTRPO

---
+++ conjugate_gradients.py
@@ -0,0 +1,35 @@
+import torch
+
+
+def conjugate_gradients(Avp, b, nsteps, residual_tol=1e-10):
+    "return approximate solution of conjugate gradient"
+    x = torch.zeros(b.size())
+    r = b - Avp(x)
+    p = r
+    rdotr = torch.dot(r, r)
+
+    for i in range(nsteps):
+        _Avp = Avp(p)
+        alpha = rdotr / torch.dot(p, _Avp)
+        x += alpha * p
+        r -= alpha * _Avp
+        new_rdotr = torch.dot(r, r)
+        betta = new_rdotr / rdotr
+        p = r + betta * p
+        rdotr = new_rdotr
+        if rdotr < residual_tol:
+            break
+    return x
+
+def flat_grad_from(net, grad_grad=False):
+    """return the flattened gradient of net."""
+
+    grads = []
+    for param in net.parameters():
+        if grad_grad:
+            grads.append(param.grad.grad.view(-1))
+        else:
+            grads.append(param.grad.view(-1))
+
+    flat_grad = torch.cat(grads)
+    return flat_grad
