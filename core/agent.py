fix return computation

core/agent.py
@@ -108,17 +108,20 @@ def _process_memory(memory, critic, gamma, tau, dtype=torch.double):
     rewards, masks, values = to_device(torch.device('cpu'), rewards, masks, values)
     tensor_type = type(rewards)
     deltas = tensor_type(rewards.size(0), 1)
+    returns = tensor_type(rewards.size(0), 1)
     advantages = torch.zeros((rewards.size(0), 1), dtype=dtype)
     prev_value = 0
     prev_advantage = 0
+    prev_return = 0
     for i in reversed(range(rewards.size(0))):
         deltas[i] = rewards[i] + gamma * prev_value * masks[i] - values[i]
+        returns[i] = rewards[i] + gamma * prev_return * masks[i]
         advantages[i] = deltas[i] + gamma * tau * prev_advantage * masks[i]
 
         prev_value = values[i, 0]
         prev_advantage = advantages[i, 0]
+        prev_return = returns[i, 0]
 
-    returns = values + advantages
     advantages = (advantages - advantages.mean()) / advantages.std()
 
     return advantages, returns, states, actions